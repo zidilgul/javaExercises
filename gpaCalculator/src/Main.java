bb91af9e6e522abd3f3c6e64dca3f63eb88799e4
Zeynep İdil Gül <z_idilgul@hotmail.com>
2022-12-21 18:22:38 +0300

    Update Main.java

--- gpaCalculator/src/Main.java
+++ gpaCalculator/src/Main.java
@@ -9,24 +9,29 @@ public class Main {
         System.out.print("Enter your mathematics grade: ");
         math = input.nextInt();
 
-        System.out.print("Enter your physics grade:");
+        System.out.print("Enter your physics grade: ");
         physics = input.nextInt();
 
-        System.out.print("Enter your chemistry grade:");
+        System.out.print("Enter your chemistry grade: ");
         chem = input.nextInt();
 
-        System.out.print("Enter your turkish grade:");
+        System.out.print("Enter your turkish grade: ");
         tr = input.nextInt();
 
-        System.out.print("Enter your history grade:");
+        System.out.print("Enter your history grade: ");
         history = input.nextInt();
 
-        System.out.print("Enter your music grade:");
+        System.out.print("Enter your music grade: ");
         music = input.nextInt();
 
             avg = math + physics + chem + tr + history + music;
             avg /=6.0;
 
         System.out.println("Your grade average is "+ avg + "!");
+
+        String state = (avg>=60)? "passed" : "failed";
+        System.out.println("You "+ state + "!");
+
+
     }
 }
