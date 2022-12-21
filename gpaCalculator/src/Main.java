f1dc0604a56bcd9b91414356da0423f668370f16
Zeynep İdil Gül <z_idilgul@hotmail.com>
2022-12-21 18:14:38 +0300

    Add files via upload

---
+++ gpaCalculator/src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        double avg;
+        int math, physics, chem, tr, history, music;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your mathematics grade: ");
+        math = input.nextInt();
+
+        System.out.print("Enter your physics grade:");
+        physics = input.nextInt();
+
+        System.out.print("Enter your chemistry grade:");
+        chem = input.nextInt();
+
+        System.out.print("Enter your turkish grade:");
+        tr = input.nextInt();
+
+        System.out.print("Enter your history grade:");
+        history = input.nextInt();
+
+        System.out.print("Enter your music grade:");
+        music = input.nextInt();
+
+            avg = math + physics + chem + tr + history + music;
+            avg /=6.0;
+
+        System.out.println("Your grade average is "+ avg + "!");
+    }
+}
