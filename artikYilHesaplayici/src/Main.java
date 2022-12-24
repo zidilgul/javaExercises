a81ad5270688d84d3f2e36b80b1911cc1ab85443
Zeynep İdil Gül <z_idilgul@hotmail.com>
2022-12-25 02:20:29 +0300

    artik yıl hesaplama programı eklendi

---
+++ artikYilHesaplayici/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year;
+        System.out.print("Yıl giriniz: ");
+        year = sc.nextInt();
+        if(year%100 == 0){
+            if (year%400 == 0)
+                System.out.println(year+" bir artık yıldır !");
+            else
+                System.out.println(year+" bir artık yıl değildir !");
+        }
+        else if (year%4 == 0 )
+            System.out.println(year+" bir artık yıldır !");
+        else
+            System.out.println(year+" bir artık yıl değildir !");
+    }
+}
