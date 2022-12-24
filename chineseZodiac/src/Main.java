7140f61732777dbd1b52b96a7b4ead27340536d0
Zeynep İdil Gül <z_idilgul@hotmail.com>
2022-12-25 02:03:26 +0300

    chinese zodiac program added

---
+++ chineseZodiac/src/Main.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year, num;
+
+        System.out.print("Enter your birth year: ");
+        year = sc.nextInt();
+
+        num = year%12;
+
+        System.out.print("Çin Zodyağı Burcunuz : ");
+        switch (num){
+            case 0:
+                System.out.println("Maymun");
+                break;
+            case 1:
+                System.out.println("Horoz");
+                break;
+            case 2:
+                System.out.println("Köpek");
+                break;
+            case 3:
+                System.out.println("Domuz");
+                break;
+            case 4:
+                System.out.println("Fare");
+                break;
+            case 5:
+                System.out.println("Öküz");
+                break;
+            case 6:
+                System.out.println("Kaplan");
+                break;
+            case 7:
+                System.out.println("Tavşan");
+                break;
+            case 8:
+                System.out.println("Ejderha");
+                break;
+            case 9:
+                System.out.println("Yılan");
+                break;
+            case 10:
+                System.out.println("At");
+                break;
+            case 11:
+                System.out.println("Koyun");
+                break;
+            default:
+                System.out.println("Hata!!");
+                break;
+        }
+    }
+}
