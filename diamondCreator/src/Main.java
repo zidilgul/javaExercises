5143d32e1fe4bdfe677830d775a923c563ebdf85
Zeynep İdil Gül <z_idilgul@hotmail.com>
2023-01-01 23:49:03 +0300

    diamond with stars program added

---
+++ diamondCreator/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner klavye = new Scanner(System.in);
+        System.out.print("Bir Sayı Giriniz :");
+        int n = klavye.nextInt();
+
+        for (int i = 0; i < n ; i++) {
+            for (int j = 0; j < (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+        for (int i = n; i >= 0 ; i--) {
+            for (int j = (n - i); j > 0; j--) {
+                System.out.print(" ");
+            }
+            for (int k = (2 * i + 1); k >= 1; k--) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
